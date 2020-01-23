0b29f42e774b270473bbf7d071fdd73ede8d8045
Zach Smith <zsmith126028@gmail.com>
2020-01-23 14:56:57 -0600

    create basic navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,54 @@ import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
-    return <nav>This is a navbar</nav>;
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-success">
+        <div className="container">
+          <Link className="text-white navbar-brand" to="#">
+            MLB API
+          </Link>
+          <button
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div id="navbarNav" className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Players
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Teams
+                </Link>
+              </li>
+            </ul>
+
+            <form className="form-inline ml-auto">
+              <input
+                type="text"
+                className="form-control mr-2"
+                placeholder="Search"
+              />
+              <button className="btn btn-outline-light">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
