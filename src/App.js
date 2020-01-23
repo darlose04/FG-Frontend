64da8bc2f6b8617e9f422b712267f05dcdd08dcd
Zach Smith <zsmith126028@gmail.com>
2020-01-23 14:00:56 -0600

    import Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1 className="text-red-700 text-5xl">Fangraphs API Frontend</h1>
       </div>
     );
