6a7ae93b32f1b3e0bb15eeef832d70ac724f4301
Zach Smith <zsmith126028@gmail.com>
2020-01-23 12:58:38 -0600

    minor edit to check if tailwind works correctly

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="text-red-700">Fangraphs API Frontend</h1>
+        <h1 className="text-red-700 text-5xl">Fangraphs API Frontend</h1>
       </div>
     );
   }
