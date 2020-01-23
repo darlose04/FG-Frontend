26f59d637b7218f8dd492ccac9aa61c60c0cb5c0
Zach Smith <zsmith126028@gmail.com>
2020-01-23 15:54:56 -0600

    bring in Home and About components. Create Switch and Routes for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layout/Navbar";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/about" render={() => <About />} />
+        </Switch>
       </div>
     );
   }
