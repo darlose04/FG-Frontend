df560f48e0144e57d8a170ca50fe493d19cd6ca5
Zach Smith <zsmith126028@gmail.com>
2020-01-24 12:53:42 -0600

    create object for team logos

---
+++ src/logos.js
@@ -0,0 +1,167 @@
+const logos = [
+  {
+    id: 1,
+    teamName: "Orioles",
+    src: "http://content.sportslogos.net/logos/53/52/thumbs/5258002019.gif"
+  },
+  {
+    id: 2,
+    teamName: "Red Sox",
+    src:
+      "http://content.sportslogos.net/logos/53/53/thumbs/c0whfsa9j0vbs079opk2s05lx.gif"
+  },
+  {
+    id: 3,
+    teamName: "White Sox",
+    src:
+      "http://content.sportslogos.net/logos/53/55/thumbs/oxvkprv7v4inf5dgqdebp0yse.gif"
+  },
+  {
+    id: 4,
+    teamName: "Indians",
+    src: "http://content.sportslogos.net/logos/53/57/thumbs/5753472014.gif"
+  },
+  {
+    id: 5,
+    teamName: "Tigers",
+    src: "http://content.sportslogos.net/logos/53/59/thumbs/5989642016.gif"
+  },
+  {
+    id: 6,
+    teamName: "Astros",
+    src: "http://content.sportslogos.net/logos/53/4929/thumbs/492995032000.gif"
+  },
+  {
+    id: 7,
+    teamName: "Royals",
+    src: "http://content.sportslogos.net/logos/53/62/thumbs/6283222019.gif"
+  },
+  {
+    id: 8,
+    teamName: "Angels",
+    src: "http://content.sportslogos.net/logos/53/6521/thumbs/652143892016.gif"
+  },
+  {
+    id: 9,
+    teamName: "Twins",
+    src:
+      "http://content.sportslogos.net/logos/53/65/thumbs/peii986yf4l42v3aa3hy0ovlf.gif"
+  },
+  {
+    id: 10,
+    teamName: "Yankees",
+    src: "http://content.sportslogos.net/logos/53/68/thumbs/1256.gif"
+  },
+  {
+    id: 11,
+    teamName: "Athletics",
+    src:
+      "http://content.sportslogos.net/logos/53/69/thumbs/6xk2lpc36146pbg2kydf13e50.gif"
+  },
+  {
+    id: 12,
+    teamName: "Mariners",
+    src: "http://content.sportslogos.net/logos/53/75/thumbs/1305.gif"
+  },
+  {
+    id: 13,
+    teamName: "Rays",
+    src: "http://content.sportslogos.net/logos/53/2535/thumbs/253594482019.gif"
+  },
+  {
+    id: 14,
+    teamName: "Rangers",
+    src:
+      "http://content.sportslogos.net/logos/53/77/thumbs/ajfeh4oqeealq37er15r3673h.gif"
+  },
+  {
+    id: 15,
+    teamName: "Blue Jays",
+    src: "http://content.sportslogos.net/logos/53/78/thumbs/7889872020.gif"
+  },
+  {
+    id: 16,
+    teamName: "Diamondbacks",
+    src:
+      "http://content.sportslogos.net/logos/54/50/thumbs/gnnnrbxcmjhdgeu6mavqk3945.gif"
+  },
+  {
+    id: 17,
+    teamName: "Braves",
+    src: "http://content.sportslogos.net/logos/54/51/thumbs/5171502018.gif"
+  },
+  {
+    id: 18,
+    teamName: "Cubs",
+    src:
+      "http://content.sportslogos.net/logos/54/54/thumbs/q9gvs07u72gc9xr3395u6jh68.gif"
+  },
+  {
+    id: 19,
+    teamName: "Reds",
+    src:
+      "http://content.sportslogos.net/logos/54/56/thumbs/z9e0rqit393ojiizsemd0t1hx.gif"
+  },
+  {
+    id: 20,
+    teamName: "Rockies",
+    src: "http://content.sportslogos.net/logos/54/58/thumbs/5868712017.gif"
+  },
+  {
+    id: 21,
+    teamName: "Dodgers",
+    src:
+      "http://content.sportslogos.net/logos/54/63/thumbs/efvfv5b5g1zgpsf56gb04lthx.gif"
+  },
+  {
+    id: 22,
+    teamName: "Marlins",
+    src: "http://content.sportslogos.net/logos/54/3637/thumbs/363775442019.gif"
+  },
+  {
+    id: 23,
+    teamName: "Brewers",
+    src: "http://content.sportslogos.net/logos/54/64/thumbs/6464742020.gif"
+  },
+  {
+    id: 24,
+    teamName: "Mets",
+    src:
+      "http://content.sportslogos.net/logos/54/67/thumbs/m01gfgeorgvbfw15fy04alujm.gif"
+  },
+  {
+    id: 25,
+    teamName: "Phillies",
+    src: ""
+  },
+  {
+    id: 26,
+    teamName: "Pirates",
+    src: "http://content.sportslogos.net/logos/54/70/thumbs/7075792019.gif"
+  },
+  {
+    id: 27,
+    teamName: "Padres",
+    src: "http://content.sportslogos.net/logos/54/73/thumbs/7375172020.gif"
+  },
+  {
+    id: 28,
+    teamName: "Giants",
+    src:
+      "http://content.sportslogos.net/logos/54/74/thumbs/cpqj6up5bvgpoedg5fwsk20ve.gif"
+  },
+  {
+    id: 29,
+    teamName: "Cardinals",
+    src:
+      "http://content.sportslogos.net/logos/54/72/thumbs/3zhma0aeq17tktge1huh7yok5.gif"
+  },
+  {
+    id: 30,
+    teamName: "Nationals",
+    src:
+      "http://content.sportslogos.net/logos/54/578/thumbs/rcehah9k0kekjkgzm077fflws.gif"
+  }
+];
+
+export { logos };
