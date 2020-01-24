fe51297870d61be48c45a4790ad331e4cd26e98b
Zach Smith <zsmith126028@gmail.com>
2020-01-24 17:16:26 -0600

    add button group for team hitting and pitching

--- src/components/teams/Teams.js
+++ src/components/teams/Teams.js
@@ -4,8 +4,19 @@ import { logos } from "../../logos";
 export default class Teams extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the teams page</h1>
+      <div className="mt-5">
+        <div
+          className="btn-group w-100"
+          role="group"
+          aria-label="Basic example"
+        >
+          <button type="button" className="btn btn-outline-success">
+            Team Hitting
+          </button>
+          <button type="button" className="btn btn-outline-success">
+            Team Pitching
+          </button>
+        </div>
       </div>
     );
   }
