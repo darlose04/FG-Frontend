6bbcaefbdc3c8c98940fb62ce11cb648ac2106f7
Zach Smith <zsmith126028@gmail.com>
2020-01-24 10:51:42 -0600

    create Teams component

---
+++ src/components/teams/Teams.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Teams extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the teams page</h1>
+      </div>
+    );
+  }
+}
