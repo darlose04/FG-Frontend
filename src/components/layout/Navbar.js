9436a5e479f0b6b30af739a396bd840d7ed61be8
Zach Smith <zsmith126028@gmail.com>
2020-01-24 10:19:14 -0600

    import Navbar style sheet

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../../styles/Navbar.css";
 
 export default class Navbar extends Component {
   render() {
