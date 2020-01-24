3ff088ff5440e087b3385891f0ddfc936ad04926
Zach Smith <zsmith126028@gmail.com>
2020-01-24 17:52:31 -0600

    update values in option element

--- src/components/teams/SeasonSelect.js
+++ src/components/teams/SeasonSelect.js
@@ -15,23 +15,23 @@ export default class SeasonSelect extends Component {
             >
               <option defaultValue>Choose Season</option>
               <option value="2019">2019</option>
-              <option value="2019">2018</option>
-              <option value="2019">2017</option>
-              <option value="2019">2016</option>
-              <option value="2019">2015</option>
-              <option value="2019">2014</option>
-              <option value="2019">2013</option>
-              <option value="2019">2012</option>
-              <option value="2019">2011</option>
-              <option value="2019">2010</option>
-              <option value="2019">2009</option>
-              <option value="2019">2008</option>
-              <option value="2019">2007</option>
-              <option value="2019">2006</option>
-              <option value="2019">2005</option>
-              <option value="2019">2004</option>
-              <option value="2019">2003</option>
-              <option value="2019">2002</option>
+              <option value="2018">2018</option>
+              <option value="2017">2017</option>
+              <option value="2016">2016</option>
+              <option value="2015">2015</option>
+              <option value="2014">2014</option>
+              <option value="2013">2013</option>
+              <option value="2012">2012</option>
+              <option value="2011">2011</option>
+              <option value="2010">2010</option>
+              <option value="2009">2009</option>
+              <option value="2008">2008</option>
+              <option value="2007">2007</option>
+              <option value="2006">2006</option>
+              <option value="2005">2005</option>
+              <option value="2004">2004</option>
+              <option value="2003">2003</option>
+              <option value="2002">2002</option>
             </select>
           </div>
           <div className="col-2 my-1">
