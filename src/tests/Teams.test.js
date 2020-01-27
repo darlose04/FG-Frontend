209e59e93a323c737784d3b7808905e680ed54c3
Zach Smith <zsmith126028@gmail.com>
2020-01-27 14:17:32 -0600

    add tests to check that components are rendering

--- src/tests/Teams.test.js
+++ src/tests/Teams.test.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { shallow, mount } from "../enzyme";
+import axios from "axios";
 
 import Teams from "../components/teams/Teams";
+import SeasonSelect from "../components/teams/SeasonSelect";
+import TeamBattingTable from "../components/teams/TeamBattingTable";
 
 // Shallow testing
 describe("<Teams />", () => {
   it("renders Teams component", () => {
     const wrapper = shallow(<Teams />);
     expect(wrapper.find(Teams)).toBeDefined();
+  });
+
+  it("renders the SeasonSelect component", () => {
+    const wrapper = shallow(<Teams />);
+    expect(wrapper.find(SeasonSelect)).toBeDefined();
+  });
+
+  it("renders the TeamBattingTable component", () => {
+    const wrapper = shallow(<Teams />);
+    expect(wrapper.find(TeamBattingTable)).toBeDefined();
   });
 });
