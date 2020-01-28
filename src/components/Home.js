8518ddfc1a5e875c71d624ed1d99ac8a49d522a2
Zach Smith <zsmith126028@gmail.com>
2020-01-28 12:16:07 -0600

    add message to Home page about website still being under construction

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,14 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>This is the home page</h1>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">THIS WEBSITE IS STILL UNDER CONSTRUCTION</h1>
+            <p class="lead">
+              Much of this site is still being developed and is incomplete.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
