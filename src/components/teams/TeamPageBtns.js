dd1f82d098755597cce46439bcf556da04ed77ce
Zach Smith <zsmith126028@gmail.com>
2020-01-28 11:39:33 -0600

    add new component for buttons on team page

---
+++ src/components/teams/TeamPageBtns.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class TeamPageBtns extends Component {
+  render() {
+    return (
+      <div className="btn-group w-100" role="group">
+        {this.props.hittingTable ? (
+          <button
+            type="button"
+            className=" teamHittingBtn btn btn-success"
+            onClick={this.props.showHittingTable}
+          >
+            Team Hitting
+          </button>
+        ) : (
+          <button
+            type="button"
+            className=" teamHittingBtn btn btn-outline-success"
+            onClick={this.props.showHittingTable}
+          >
+            Team Hitting
+          </button>
+        )}
+
+        <button
+          type="button"
+          className="teamPitchingBtn btn btn-outline-success"
+        >
+          Team Pitching
+        </button>
+      </div>
+    );
+  }
+}
