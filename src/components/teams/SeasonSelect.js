e0d8778f970c018874cb7b8ef1b6aaf8fc464628
Zach Smith <zsmith126028@gmail.com>
2020-01-28 14:13:13 -0600

    add buttons that will distinguish between standard and advanced stats

--- src/components/teams/SeasonSelect.js
+++ src/components/teams/SeasonSelect.js
@@ -23,47 +23,61 @@ export default class SeasonSelect extends Component {
 
   render() {
     return (
-      <form className="seasonSelectForm" onSubmit={this.seasonSubmit}>
-        <div className="form-row align-items-center">
-          <div className="col-4 my-3">
-            <label className="mr-sm-2 sr-only" htmlFor="seasonSelectHitting">
-              Preference
-            </label>
-            <select
-              className="custom-select mr-sm-2"
-              id="seasonSelectHitting"
-              onChange={this.seasonChange}
-            >
-              <option defaultValue={this.props.season}>
-                {this.props.season}
-              </option>
-              <option value="2019">2019</option>
-              <option value="2018">2018</option>
-              <option value="2017">2017</option>
-              <option value="2016">2016</option>
-              <option value="2015">2015</option>
-              <option value="2014">2014</option>
-              <option value="2013">2013</option>
-              <option value="2012">2012</option>
-              <option value="2011">2011</option>
-              <option value="2010">2010</option>
-              <option value="2009">2009</option>
-              <option value="2008">2008</option>
-              <option value="2007">2007</option>
-              <option value="2006">2006</option>
-              <option value="2005">2005</option>
-              <option value="2004">2004</option>
-              <option value="2003">2003</option>
-              <option value="2002">2002</option>
-            </select>
+      <div>
+        <form className="seasonSelectForm" onSubmit={this.seasonSubmit}>
+          <div className="form-row align-items-center">
+            <div className="col-8 my-3">
+              <label className="mr-sm-2 sr-only" htmlFor="seasonSelectHitting">
+                Preference
+              </label>
+              <select
+                className="custom-select mr-sm-2"
+                id="seasonSelectHitting"
+                onChange={this.seasonChange}
+              >
+                <option defaultValue={this.props.season}>
+                  {this.props.season}
+                </option>
+                <option value="2019">2019</option>
+                <option value="2018">2018</option>
+                <option value="2017">2017</option>
+                <option value="2016">2016</option>
+                <option value="2015">2015</option>
+                <option value="2014">2014</option>
+                <option value="2013">2013</option>
+                <option value="2012">2012</option>
+                <option value="2011">2011</option>
+                <option value="2010">2010</option>
+                <option value="2009">2009</option>
+                <option value="2008">2008</option>
+                <option value="2007">2007</option>
+                <option value="2006">2006</option>
+                <option value="2005">2005</option>
+                <option value="2004">2004</option>
+                <option value="2003">2003</option>
+                <option value="2002">2002</option>
+              </select>
+            </div>
+            <div className="col-4 my-1">
+              <button type="submit" className="btn btn-success w-100">
+                Submit
+              </button>
+            </div>
           </div>
-          <div className="col-2 my-1">
-            <button type="submit" className="btn btn-success w-100">
-              Submit
-            </button>
-          </div>
+        </form>
+        <div
+          className="btn-group w-100"
+          role="group"
+          aria-label="Basic example"
+        >
+          <button type="button" className="btn btn-outline-success">
+            Standard
+          </button>
+          <button type="button" className="btn btn-outline-success">
+            Advanced
+          </button>
         </div>
-      </form>
+      </div>
     );
   }
 }
