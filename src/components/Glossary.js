0181af0e4fcf2a0cb670f200eea088559d1644f1
Zach Smith <zsmith126028@gmail.com>
2020-01-28 13:55:54 -0600

    add new component for a glossary page

---
+++ src/components/Glossary.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Glossary extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the glossary page where stats will be explained</h1>
+      </div>
+    );
+  }
+}
