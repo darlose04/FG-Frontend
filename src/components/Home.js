d7e4b1ee5bf57dd26476dd4d8d492e73e6492376
Zach Smith <zsmith126028@gmail.com>
2020-01-28 13:37:09 -0600

    change class to className

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,12 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div class="jumbotron jumbotron-fluid">
-          <div class="container">
-            <h1 class="display-4">THIS WEBSITE IS STILL UNDER CONSTRUCTION</h1>
-            <p class="lead">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4">
+              THIS WEBSITE IS STILL UNDER CONSTRUCTION
+            </h1>
+            <p className="lead">
               Much of this site is still being developed and is incomplete.
             </p>
           </div>
