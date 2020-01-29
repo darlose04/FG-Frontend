2e23ba2637a1d41976ccba3fe97c5e2a85877b89
Zach Smith <zsmith126028@gmail.com>
2020-01-29 13:59:33 -0600

    add initial directory and files for pitching

---
+++ src/components/teams/pitching/TeamAdvancedPitchingTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TeamAdvancedPitchingTable extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Advanced Table</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/teams/pitching/TeamStandardPitchingTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TeamStandardPitchingTable extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Standard Table</h1>
+      </div>
+    );
+  }
+}
