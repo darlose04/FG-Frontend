7f1fb12491931b3302e846b8856f502ba1062bbc
Zach Smith <zsmith126028@gmail.com>
2020-01-30 11:44:31 -0600

    add props and onClicks for buttons

--- src/components/teams/pitching/PitchingButtons.js
+++ src/components/teams/pitching/PitchingButtons.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 
 export default class PitchingButtons extends Component {
   render() {
-    let { totalsClass, startersClass, relieversClass } = this.props;
+    let {
+      totalsClass,
+      startersClass,
+      relieversClass,
+      showTotals,
+      showStarting,
+      showRelieving
+    } = this.props;
     return (
       <div className="btn-group w-100 mb-3" role="group">
-        <button type="button" className={totalsClass} onClick={}>
+        <button type="button" className={totalsClass} onClick={showTotals}>
           Totals
         </button>
-        <button type="button" className={startersClass} onClick={}>
+        <button type="button" className={startersClass} onClick={showStarting}>
           Starters
         </button>
-        <button type="button" className={relieversClass} onClick={}>
+        <button
+          type="button"
+          className={relieversClass}
+          onClick={showRelieving}
+        >
           Relievers
         </button>
       </div>
