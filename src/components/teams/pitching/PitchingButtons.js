2405769d073bc0c1feb68d9f7cf3fc9dbf1f7447
Zach Smith <zsmith126028@gmail.com>
2020-01-29 20:14:47 -0600

    bring in props for className state

--- src/components/teams/pitching/PitchingButtons.js
+++ src/components/teams/pitching/PitchingButtons.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 
 export default class PitchingButtons extends Component {
   render() {
+    let { totalsClass, startersClass, relieversClass } = this.props;
     return (
       <div className="btn-group w-100 mb-3" role="group">
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className={totalsClass} onClick={}>
           Totals
         </button>
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className={startersClass} onClick={}>
           Starters
         </button>
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className={relieversClass} onClick={}>
           Relievers
         </button>
       </div>
