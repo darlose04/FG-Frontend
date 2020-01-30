e24536cd6e796792e440db6076cf92ea55cd883a
Zach Smith <zsmith126028@gmail.com>
2020-01-30 13:41:32 -0600

    add tests for rendering TeamPageBtns

--- src/tests/teams/TeamPageBtns.test.js
+++ src/tests/teams/TeamPageBtns.test.js
@@ -4,3 +4,14 @@ import { shallow } from "../../enzyme";
 import TeamPageBtns from "../../components/teams/TeamPageBtns";
 
 // Shallow testing
+describe("<TeamPageBtns />", () => {
+  it("renders the TeamPageBtns component", () => {
+    const wrapper = shallow(<TeamPageBtns />);
+    expect(wrapper.find(TeamPageBtns)).toBeDefined();
+  });
+
+  it("renders two buttons", () => {
+    const wrapper = shallow(<TeamPageBtns />);
+    expect(wrapper.find("button")).toHaveLength(2);
+  });
+});
