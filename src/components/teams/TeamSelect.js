d402f423e3b104d0209477cedf17c339ddddb848
Zach Smith <zsmith126028@gmail.com>
2020-02-03 14:31:28 -0600

    add AL East team logos in cards

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
-import logos from "../../logos";
+import sortedLogos from "../../logos";
 
 export default class TeamSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teamInfo: sortedLogos
+    };
+  }
+
   render() {
+    console.log(this.state.teamInfo);
+    const logos = this.state.teamInfo.sortedLogos;
     return (
-      <div>
-        <p>Team Select Page</p>
+      <div className="container">
+        <h2 className="mt-4">American League East</h2>
+        <div className="row-hl d-flex justify-content-between">
+          {logos.map(team =>
+            team.division === "AL East" ? (
+              <div className="p-2 item-hl">
+                <div className="card" style={{ width: "10rem" }}>
+                  <img
+                    src={team.src}
+                    className="card-img-top mt-1"
+                    alt={team.teamName}
+                  ></img>
+                  <div className="card-body pb-1">
+                    <h4 className="card-title text-center">{team.teamName}</h4>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )
+          )}
+        </div>
       </div>
     );
   }
