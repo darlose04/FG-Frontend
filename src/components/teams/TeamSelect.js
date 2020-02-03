40a7de0c372690d4e69bb40318544047a929b18b
Zach Smith <zsmith126028@gmail.com>
2020-02-03 13:01:23 -0600

    add initial TeamSelect component for selecting teams

---
+++ src/components/teams/TeamSelect.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TeamSelect extends Component {
+  render() {
+    return (
+      <div>
+        <p>Team Select Page</p>
+      </div>
+    );
+  }
+}
