ce85a88403e47422abe6d53b04e38f76089c5b0e
Zach Smith <zsmith126028@gmail.com>
2020-02-04 14:08:26 -0600

    remove function that is no longer needed

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -4,27 +4,6 @@ export default class TeamSelect extends Component {
   render() {
     const logos = this.props.logos.logos;
 
-    const teamDisplay = () => {
-      logos.map(logo => {
-        let divisionKey = Object.keys(logo)[0];
-        // console.log(logo[divisionKey]);
-
-        return logo[divisionKey].map(team => (
-          <div className="row-hl d-flex justify-content-between mt-4">
-            <div className="p-2 item-hl" key={team.id}>
-              <div className="card" style={{ width: "10rem" }}>
-                <img
-                  src={team.src}
-                  className="card-img-top p-2"
-                  alt={team.teamName}
-                ></img>
-              </div>
-            </div>
-          </div>
-        ));
-      });
-    };
-
     return (
       <div className="container mb-5">
         {logos.map(logo => {
