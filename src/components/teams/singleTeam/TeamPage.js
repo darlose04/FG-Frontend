e856b535f2c1d52db3301d9bdad0a92eb04d4449
Zach Smith <zsmith126028@gmail.com>
2020-02-04 15:26:39 -0600

    add state. add componentDidMount for api call

--- src/components/teams/singleTeam/TeamPage.js
+++ src/components/teams/singleTeam/TeamPage.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
+const baseUrl = "https://www.fgbaseballapi.com/api";
 export default class TeamPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      team: "",
+      batting: [],
+      pitching: [],
+      starting: [],
+      relieving: []
+    };
+  }
+
+  async componentDidMount() {
+    const teamName = this.props.match.params.team;
+
+    let batting = await axios.get(`${baseUrl}/teambatting/teams/${teamName}`);
+    let sortedBatting = batting.data.sort((a, b) => {
+      return b.season - a.season;
+    });
+
+    this.setState({
+      team: teamName,
+      batting: sortedBatting
+    });
   }
 
   render() {
