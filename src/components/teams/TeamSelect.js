bd988fb31a63eeada528e6ca412d029730ee265b
Zach Smith <zsmith126028@gmail.com>
2020-02-04 14:30:27 -0600

    add Link to individual team page

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "../../styles/TeamSelect.css";
 
 export default class TeamSelect extends Component {
@@ -15,13 +16,15 @@ export default class TeamSelect extends Component {
             <div className="row-hl d-flex justify-content-between mt-4">
               {logo[divisionKey].map(team => (
                 <div className="p-2 item-hl" key={team.id}>
-                  <div className="card teamImg">
-                    <img
-                      src={team.src}
-                      className="card-img-top p-2 "
-                      alt={team.teamName}
-                    ></img>
-                  </div>
+                  <Link to={`/teamselect/${team.teamName}`}>
+                    <div className="card teamImg">
+                      <img
+                        src={team.src}
+                        className="card-img-top p-2 "
+                        alt={team.teamName}
+                      ></img>
+                    </div>
+                  </Link>
                 </div>
               ))}
             </div>
