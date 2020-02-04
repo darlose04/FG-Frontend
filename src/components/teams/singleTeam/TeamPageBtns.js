bfcd040580e74508ea5d5bb6dcac2409dea237b3
Zach Smith <zsmith126028@gmail.com>
2020-02-04 17:02:33 -0600

    add margin bottom

--- src/components/teams/singleTeam/TeamPageBtns.js
+++ src/components/teams/singleTeam/TeamPageBtns.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class TeamPageBtns extends Component {
   render() {
     return (
-      <div className="btn-group w-100" role="group">
+      <div className="btn-group w-100 mb-3" role="group">
         <button type="button" className="btn btn-outline-success">
           Hitting
         </button>
