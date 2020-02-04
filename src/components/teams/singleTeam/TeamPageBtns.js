8925f1d4b4ca197d91099bea27e6aacb0eeb88cd
Zach Smith <zsmith126028@gmail.com>
2020-02-04 17:00:26 -0600

    create buttons component for the individual team pages

---
+++ src/components/teams/singleTeam/TeamPageBtns.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class TeamPageBtns extends Component {
+  render() {
+    return (
+      <div className="btn-group w-100" role="group">
+        <button type="button" className="btn btn-outline-success">
+          Hitting
+        </button>
+        <button type="button" className="btn btn-outline-success">
+          Pitching
+        </button>
+      </div>
+    );
+  }
+}
