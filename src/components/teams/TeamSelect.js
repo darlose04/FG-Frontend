7723c0787eba3590d72538b42d1d4ad650f429f3
Zach Smith <zsmith126028@gmail.com>
2020-02-03 18:23:16 -0600

    trying to loop through an array of objects that each have an array of objects with map

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -5,32 +5,35 @@ export default class TeamSelect extends Component {
     const logos = this.props.logos.logos;
 
     const teamDisplay = () => {
-      for (let i = 0; i < logos.length; i++) {
-        console.log(Object.keys(logos[i])[0]);
-        console.log(Object.values(logos[i])[0][0].teamName);
-      }
+      logos.map(logo => {
+        let divisionKey = Object.keys(logo)[0];
+        // console.log(logo[divisionKey]);
+
+        return logo[divisionKey].map(team => (
+          <div className="row-hl d-flex justify-content-between mt-4">
+            <div className="p-2 item-hl" key={team.id}>
+              <div className="card" style={{ width: "10rem" }}>
+                <img
+                  src={team.src}
+                  className="card-img-top p-2"
+                  alt={team.teamName}
+                ></img>
+              </div>
+            </div>
+          </div>
+        ));
+      });
     };
-    teamDisplay();
+
     return (
       <div className="container mb-5">
-        {/* {logos.map(division => (
-        <div className="row-hl d-flex justify-content-between mt-4">
-        <div className="p-2 item-hl">
-                <div className="card" style={{ width: "10rem" }}>
-                  <img
-                    src={team.src}
-                    className="card-img-top p-2"
-                    alt={team.teamName}
-                  ></img>
-                </div>
-              </div>
-        </div>
-      ))} */}
+        {logos.map(logo => {
+          let divisionKey = Object.keys(logo)[0];
+          // console.log(logo[divisionKey]);
 
-        {/* <div className="row-hl d-flex justify-content-between mt-4">
-          {logos.map(team =>
-            team.division === "AL East" ? (
-              <div className="p-2 item-hl">
+          return logo[divisionKey].map(team => (
+            <div className="row-hl d-flex justify-content-between mt-4">
+              <div className="p-2 item-hl" key={team.id}>
                 <div className="card" style={{ width: "10rem" }}>
                   <img
                     src={team.src}
@@ -39,96 +42,9 @@ export default class TeamSelect extends Component {
                   ></img>
                 </div>
               </div>
-            ) : (
-              <div></div>
-            )
-          )}
-        </div>
-        <div className="row-hl d-flex justify-content-between">
-          {logos.map(team =>
-            team.division === "AL Central" ? (
-              <div className="p-2 item-hl">
-                <div className="card" style={{ width: "10rem" }}>
-                  <img
-                    src={team.src}
-                    className="card-img-top p-2"
-                    alt={team.teamName}
-                  ></img>
-                </div>
-              </div>
-            ) : (
-              <div></div>
-            )
-          )}
-        </div>
-        <div className="row-hl d-flex justify-content-between">
-          {logos.map(team =>
-            team.division === "AL West" ? (
-              <div className="p-2 item-hl">
-                <div className="card" style={{ width: "10rem" }}>
-                  <img
-                    src={team.src}
-                    className="card-img-top p-2"
-                    alt={team.teamName}
-                  ></img>
-                </div>
-              </div>
-            ) : (
-              <div></div>
-            )
-          )}
-        </div>
-        <div className="row-hl d-flex justify-content-between">
-          {logos.map(team =>
-            team.division === "NL East" ? (
-              <div className="p-2 item-hl">
-                <div className="card" style={{ width: "10rem" }}>
-                  <img
-                    src={team.src}
-                    className="card-img-top p-2"
-                    alt={team.teamName}
-                  ></img>
-                </div>
-              </div>
-            ) : (
-              <div></div>
-            )
-          )}
-        </div>
-        <div className="row-hl d-flex justify-content-between">
-          {logos.map(team =>
-            team.division === "NL Central" ? (
-              <div className="p-2 item-hl">
-                <div className="card" style={{ width: "10rem" }}>
-                  <img
-                    src={team.src}
-                    className="card-img-top p-2"
-                    alt={team.teamName}
-                  ></img>
-                </div>
-              </div>
-            ) : (
-              <div></div>
-            )
-          )}
-        </div>
-        <div className="row-hl d-flex justify-content-between">
-          {logos.map(team =>
-            team.division === "NL West" ? (
-              <div className="p-2 item-hl">
-                <div className="card" style={{ width: "10rem" }}>
-                  <img
-                    src={team.src}
-                    className="card-img-top p-2"
-                    alt={team.teamName}
-                  ></img>
-                </div>
-              </div>
-            ) : (
-              <div></div>
-            )
-          )}
-        </div>*/}
+            </div>
+          ));
+        })}
       </div>
     );
   }
