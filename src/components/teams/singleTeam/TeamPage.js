066ca175d548657e8434105fd0b118a0fd6e6ead
Zach Smith <zsmith126028@gmail.com>
2020-02-04 14:41:17 -0600

    add link to return to team selection, get team name from parameter

--- src/components/teams/singleTeam/TeamPage.js
+++ src/components/teams/singleTeam/TeamPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class TeamPage extends Component {
   constructor(props) {
@@ -6,9 +7,16 @@ export default class TeamPage extends Component {
   }
 
   render() {
+    const teamName = this.props.match.params.team;
+
     return (
-      <div>
-        <h1>This is the team page for individual team stats</h1>
+      <div className="mt-4">
+        <Link to="/teamselect">
+          <button type="button" className="btn btn-success">
+            Return to team selection
+          </button>
+        </Link>
+        <h1>This is the team page for the {teamName}</h1>
       </div>
     );
   }
