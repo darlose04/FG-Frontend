5817f45f0291bea16f53f849c37dec3879533fb8
Zach Smith <zsmith126028@gmail.com>
2020-02-04 14:25:27 -0600

    remove Route and Switch import, just need to use Links on this page

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
 import "../../styles/TeamSelect.css";
 
 export default class TeamSelect extends Component {
