85f3e641bb776572fd5e6a195e828dbb5cd8fba4
Zach Smith <zsmith126028@gmail.com>
2020-02-04 14:32:56 -0600

    create initial constructor

--- src/components/teams/singleTeam/TeamPage.js
+++ src/components/teams/singleTeam/TeamPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class TeamPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
