ced77c08ce4ba428e1922627820bf949ae8864c2
Zach Smith <zsmith126028@gmail.com>
2020-02-06 14:36:52 -0600

    attempting to test TeamPage component, but not sure how to pass param props into test

--- src/tests/teams/singleTeamTests/TeamPage.test.js
+++ src/tests/teams/singleTeamTests/TeamPage.test.js
@@ -3,6 +3,16 @@ import { shallow, mount } from "../../../enzyme";
 import sinon from "sinon";
 import axios from "axios";
 
+import TeamPage from "../../../components/teams/singleTeam/TeamPage";
 import TeamPageBtns from "../../../components/teams/singleTeam/TeamPageBtns";
 import StdBattingTable from "../../../components/teams/singleTeam/batting/StdBattingTable";
 import AdvBattingTable from "../../../components/teams/singleTeam/batting/AdvBattingTable";
+
+describe("TeamPage component", () => {
+  it("renders the TeamPage component", () => {
+    const wrapper = shallow(<TeamPage />);
+    expect(wrapper.find(TeamPage)).toBeDefined();
+  });
+
+  it("renders the TeamPageBtns", () => {});
+});
