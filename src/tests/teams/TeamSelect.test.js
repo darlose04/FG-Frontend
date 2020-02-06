79952c15033f152d30e740e274380c07a4c3dbaf
Zach Smith <zsmith126028@gmail.com>
2020-02-06 16:10:52 -0600

    create initial test file for TeamSelect component

---
+++ src/tests/teams/TeamSelect.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { shallow, mount } from "../../enzyme";
+import logos from "../../logos";
+
+import TeamSelect from "../../components/teams/TeamSelect";
+
+describe("TeamSelect component", () => {});
