ccc929b794e1a091e3a3abfa71c060d2dab6fa92
Zach Smith <zsmith126028@gmail.com>
2020-02-06 14:08:53 -0600

    add classnames to buttons for enzyme test

--- src/components/teams/singleTeam/TeamPageBtns.js
+++ src/components/teams/singleTeam/TeamPageBtns.js
@@ -4,10 +4,10 @@ export default class TeamPageBtns extends Component {
   render() {
     return (
       <div className="btn-group w-100 mb-3" role="group">
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className="btn btn-outline-success hittingBtn">
           Hitting
         </button>
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className="btn btn-outline-success pitchingBtn">
           Pitching
         </button>
       </div>
