f500594db109cca9e74bc5d26fc4e7a032c40070
Zach Smith <zsmith126028@gmail.com>
2020-02-06 14:24:46 -0600

    create initial test file for TeamPage component

---
+++ src/tests/teams/singleTeamTests/TeamPage.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow, mount } from "../../../enzyme";
+import sinon from "sinon";
+import axios from "axios";
+
+import TeamPageBtns from "../../../components/teams/singleTeam/TeamPageBtns";
+import StdBattingTable from "../../../components/teams/singleTeam/batting/StdBattingTable";
+import AdvBattingTable from "../../../components/teams/singleTeam/batting/AdvBattingTable";
