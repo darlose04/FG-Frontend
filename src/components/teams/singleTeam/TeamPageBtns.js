68d4a9c4980591008cf9f21ee7a6def80fd24a09
Zach Smith <zsmith126028@gmail.com>
2020-02-07 10:40:07 -0600

    add props to onClick function

--- src/components/teams/singleTeam/TeamPageBtns.js
+++ src/components/teams/singleTeam/TeamPageBtns.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 
 export default class TeamPageBtns extends Component {
   render() {
-    const { hittingBtnClass, pitchingBtnClass } = this.props;
+    const {
+      hittingBtnClass,
+      pitchingBtnClass,
+      showHitting,
+      showPitching
+    } = this.props;
 
     return (
       <div className="btn-group w-100 mb-3" role="group">
-        <button type="button" className={hittingBtnClass}>
+        <button type="button" className={hittingBtnClass} onClick={showHitting}>
           Hitting
         </button>
-        <button type="button" className={pitchingBtnClass}>
+        <button
+          type="button"
+          className={pitchingBtnClass}
+          onClick={showPitching}
+        >
           Pitching
         </button>
       </div>
