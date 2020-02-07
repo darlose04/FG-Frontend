e813a29491d398b42ae759e03324b5cebdaf86d6
Zach Smith <zsmith126028@gmail.com>
2020-02-07 10:29:14 -0600

    use props as className

--- src/components/teams/singleTeam/TeamPageBtns.js
+++ src/components/teams/singleTeam/TeamPageBtns.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 export default class TeamPageBtns extends Component {
   render() {
+    const { hittingBtnClass, pitchingBtnClass } = this.props;
+
     return (
       <div className="btn-group w-100 mb-3" role="group">
-        <button type="button" className="btn btn-outline-success hittingBtn">
+        <button type="button" className={hittingBtnClass}>
           Hitting
         </button>
-        <button type="button" className="btn btn-outline-success pitchingBtn">
+        <button type="button" className={pitchingBtnClass}>
           Pitching
         </button>
       </div>
