9515d32dfb838420c0dee24b3f0f27526f815f94
Zach Smith <zsmith126028@gmail.com>
2020-02-07 17:35:13 -0600

    finish component for showing total stats tables

--- src/components/teams/singleTeam/pitching/TotalPitchingComponent.js
+++ src/components/teams/singleTeam/pitching/TotalPitchingComponent.js
@@ -4,9 +4,40 @@ import AdvPitchingTable from "./AdvPitchingTable";
 
 export default class TotalPitchingComponent extends Component {
   render() {
+    const {
+      std,
+      adv,
+      data,
+      showStd,
+      showAdv,
+      stdClass,
+      advClass,
+      clickStyle
+    } = this.props;
     return (
       <div>
         <h2>Totals</h2>
+        {std ? (
+          <StdPitchingTable
+            stats={data}
+            showStd={showStd}
+            showAdv={showAdv}
+            stdClass={stdClass}
+            advClass={advClass}
+            clickStyle={clickStyle}
+          />
+        ) : adv ? (
+          <AdvPitchingTable
+            stats={data}
+            showStd={showStd}
+            showAdv={showAdv}
+            stdClass={stdClass}
+            advClass={advClass}
+            clickStyle={clickStyle}
+          />
+        ) : (
+          <div></div>
+        )}
       </div>
     );
   }
