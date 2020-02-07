2617033b280cb03e9dc57c47f1107912ad8f4a87
Zach Smith <zsmith126028@gmail.com>
2020-02-07 17:25:57 -0600

    create initial component for total pitching data

---
+++ src/components/teams/singleTeam/pitching/TotalPitchingComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import StdPitchingTable from "./StdPitchingTable";
+import AdvPitchingTable from "./AdvPitchingTable";
+
+export default class TotalPitchingComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Totals</h2>
+      </div>
+    );
+  }
+}
