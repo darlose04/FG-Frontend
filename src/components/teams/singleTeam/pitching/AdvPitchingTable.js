600789db94adfc8e92961fbd802d25d9f38d4b4e
Zach Smith <zsmith126028@gmail.com>
2020-02-07 12:37:36 -0600

    add another th to adv table

--- src/components/teams/singleTeam/pitching/AdvPitchingTable.js
+++ src/components/teams/singleTeam/pitching/AdvPitchingTable.js
@@ -9,6 +9,7 @@ export default class AdvPitchingTable extends Component {
         style={{ fontSize: "0.8rem", padding: ".2rem", whiteSpace: "nowrap" }}
       >
         <thead>
+          <th className="bg-success text-white">Standard</th>
           <th className="bg-success text-white">Advanced</th>
         </thead>
         <thead>
