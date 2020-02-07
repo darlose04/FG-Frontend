62254551e5e04b4bacd16f8d9ad75c21d8d8421f
Zach Smith <zsmith126028@gmail.com>
2020-02-07 17:23:05 -0600

    add new component to hold the batting table info in order to reduce the amount of code in the TeamPage component

---
+++ src/components/teams/singleTeam/batting/BattingComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import StdBattingTable from "./StdBattingTable";
+import AdvBattingTable from "./AdvBattingTable";
+
+export default class BattingComponent extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div>
+        <StdBattingTable stats={data} />
+        <AdvBattingTable stats={data} />
+      </div>
+    );
+  }
+}
