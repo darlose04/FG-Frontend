87f7a309803763ddea6b61fb3ad0c7f6759ae126
Zach Smith <zsmith126028@gmail.com>
2020-02-10 09:17:29 -0600

    trying to refactor TeamSelect displays based on new logos array

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -4,32 +4,25 @@ import "../../styles/TeamSelect.css";
 
 export default class TeamSelect extends Component {
   render() {
-    const logos = this.props.logos.logos;
-
+    const { logos } = this.props.logos;
+    console.log(logos);
     return (
       <div className="container mb-5">
-        {logos.map(logo => {
-          let divisionKey = Object.keys(logo)[0];
-          // console.log(logo[divisionKey]);
-
-          return (
-            <div className="row-hl d-flex justify-content-between mt-4">
-              {logo[divisionKey].map(team => (
-                <div className="p-2 item-hl" key={team.id}>
-                  <Link to={`/teamselect/${team.teamName}`}>
-                    <div className="card teamImg">
-                      <img
-                        src={team.src}
-                        className="card-img-top p-2 "
-                        alt={team.teamName}
-                      ></img>
-                    </div>
-                  </Link>
+        {logos.map(team => (
+          <div className="row-hl d-flex justify-content-between mt-4">
+            <div className="p-2 item-hl" key={team.id}>
+              <Link to={`/teamselect/${team.teamName}`}>
+                <div className="card teamImg">
+                  <img
+                    src={team.src}
+                    alt={team.teamName}
+                    className="card-img-top p-2"
+                  />
                 </div>
-              ))}
+              </Link>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     );
   }
