9810769063d919f402c8eb6a216b82182528073c
Zach Smith <zsmith126028@gmail.com>
2020-02-10 11:10:57 -0600

    refactor logo displays based on new logos file. add new array to push filtered arrays into, then map those

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -5,41 +5,36 @@ import "../../styles/TeamSelect.css";
 export default class TeamSelect extends Component {
   render() {
     const { logos } = this.props.logos;
+    let divisions = [];
+    const alEast = logos.filter(team => team.division === "AL East");
+    const alCentral = logos.filter(team => team.division === "AL Central");
+    const alWest = logos.filter(team => team.division === "AL West");
+    const nlEast = logos.filter(team => team.division === "NL East");
+    const nlCentral = logos.filter(team => team.division === "NL Central");
+    const nlWest = logos.filter(team => team.division === "NL West");
+    divisions.push(alEast, alCentral, alWest, nlEast, nlCentral, nlWest);
 
     return (
-      <div className="container mt-4">
-        <div className="row logoRows">
-          {logos.map(team => (
-            <div className="col-2 p-2">
-              <Link to={`/teamselect/${team.teamName}`}>
-                <div className="card teamImg">
-                  <img
-                    src={team.src}
-                    alt={team.teamName}
-                    className="card-img-top p-2"
-                  />
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-        {/* {logos.map(team => (
+      <div className="container my-2 ">
+        {divisions.map(division => (
           <div className="row-hl d-flex justify-content-between mt-4">
-            <div className="p-2 item-hl row" key={team.id}>
-              <div className="col-sm">
-                <Link to={`/teamselect/${team.teamName}`}>
-                  <div className="card teamImg">
-                    <img
-                      src={team.src}
-                      alt={team.teamName}
-                      className="card-img-top p-2"
-                    />
-                  </div>
-                </Link>
+            {division.map(team => (
+              <div className="p-2 item-hl row" key={team.id}>
+                <div className="col-sm">
+                  <Link to={`/teamselect/${team.teamName}`}>
+                    <div className="card teamImg">
+                      <img
+                        src={team.src}
+                        alt={team.teamName}
+                        className="card-img-top p-2"
+                      />
+                    </div>
+                  </Link>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))} */}
+        ))}
       </div>
     );
   }
