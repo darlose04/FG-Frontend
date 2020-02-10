4ea60ab56b9dce2b1195a4ef9e54416bd5b8d139
Zach Smith <zsmith126028@gmail.com>
2020-02-10 13:05:28 -0600

    create initial file that will hold stats for single seasons and the roster of that season

---
+++ src/components/teams/singleTeam/SeasonRosterStats.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SeasonRosterStats extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single season and Roster stats will go here</h1>
+      </div>
+    );
+  }
+}
