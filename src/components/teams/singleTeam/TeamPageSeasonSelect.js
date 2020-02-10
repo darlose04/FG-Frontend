d4055892fca19a5b3d64267a22f9f30e0d5844fc
Zach Smith <zsmith126028@gmail.com>
2020-02-10 13:33:13 -0600

    add list of season and then map through it for button display

--- src/components/teams/singleTeam/TeamPageSeasonSelect.js
+++ src/components/teams/singleTeam/TeamPageSeasonSelect.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 
 export default class TeamPageSeasonSelect extends Component {
   render() {
+    const seasonList = [
+      2002,
+      2003,
+      2004,
+      2005,
+      2006,
+      2007,
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+      2013,
+      2014,
+      2015,
+      2016,
+      2017,
+      2018,
+      2019
+    ];
+
     return (
       <div
         className="btn-toolbar my-2"
@@ -9,133 +30,15 @@ export default class TeamPageSeasonSelect extends Component {
         aria-label="Toolbar with button groups"
       >
         <div className="btn-group w-100" role="group" aria-label="First group">
-          <button
-            type="button"
-            value="2002"
-            className="btn btn-outline-success"
-          >
-            2002
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2003"
-          >
-            2003
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2004"
-          >
-            2004
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2005"
-          >
-            2005
-          </button>
-
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2006"
-          >
-            2006
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2007"
-          >
-            2007
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2008"
-          >
-            2008
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2009"
-          >
-            2009
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2010"
-          >
-            2010
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2011"
-          >
-            2011
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2012"
-          >
-            2012
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2013"
-          >
-            2013
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2014"
-          >
-            2014
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2015"
-          >
-            2015
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2016"
-          >
-            2016
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2017"
-          >
-            2017
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2018"
-          >
-            2018
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-success"
-            value="2019"
-          >
-            2019
-          </button>
+          {seasonList.map(season => (
+            <button
+              type="button"
+              value={season}
+              className="btn btn-outline-success"
+            >
+              {season}
+            </button>
+          ))}
         </div>
       </div>
     );
