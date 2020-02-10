afcea61c3dbabf24bc115f028f2be0b68c73370c
Zach Smith <zsmith126028@gmail.com>
2020-02-10 13:03:00 -0600

    add values to buttons

--- src/components/teams/singleTeam/TeamPageSeasonSelect.js
+++ src/components/teams/singleTeam/TeamPageSeasonSelect.js
@@ -9,59 +9,131 @@ export default class TeamPageSeasonSelect extends Component {
         aria-label="Toolbar with button groups"
       >
         <div className="btn-group w-100" role="group" aria-label="First group">
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            value="2002"
+            className="btn btn-outline-success"
+          >
             2002
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2003"
+          >
             2003
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2004"
+          >
             2004
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2005"
+          >
             2005
           </button>
 
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2006"
+          >
             2006
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2007"
+          >
             2007
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2008"
+          >
             2008
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2009"
+          >
             2009
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2010"
+          >
             2010
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2011"
+          >
             2011
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2012"
+          >
             2012
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2013"
+          >
             2013
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2014"
+          >
             2014
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2015"
+          >
             2015
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2016"
+          >
             2016
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2017"
+          >
             2017
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2018"
+          >
             2018
           </button>
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            value="2019"
+          >
             2019
           </button>
         </div>
