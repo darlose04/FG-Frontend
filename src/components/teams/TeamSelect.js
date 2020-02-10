e124a306073b77eba0603220f4c4eb6ec906b765
Zach Smith <zsmith126028@gmail.com>
2020-02-10 09:34:23 -0600

    refactored to a point, just need to only have five teams per row

--- src/components/teams/TeamSelect.js
+++ src/components/teams/TeamSelect.js
@@ -5,12 +5,12 @@ import "../../styles/TeamSelect.css";
 export default class TeamSelect extends Component {
   render() {
     const { logos } = this.props.logos;
-    console.log(logos);
+
     return (
-      <div className="container mb-5">
-        {logos.map(team => (
-          <div className="row-hl d-flex justify-content-between mt-4">
-            <div className="p-2 item-hl" key={team.id}>
+      <div className="container mt-4">
+        <div className="row">
+          {logos.map(team => (
+            <div className="col-2 p-2">
               <Link to={`/teamselect/${team.teamName}`}>
                 <div className="card teamImg">
                   <img
@@ -21,8 +21,25 @@ export default class TeamSelect extends Component {
                 </div>
               </Link>
             </div>
+          ))}
+        </div>
+        {/* {logos.map(team => (
+          <div className="row-hl d-flex justify-content-between mt-4">
+            <div className="p-2 item-hl row" key={team.id}>
+              <div className="col-sm">
+                <Link to={`/teamselect/${team.teamName}`}>
+                  <div className="card teamImg">
+                    <img
+                      src={team.src}
+                      alt={team.teamName}
+                      className="card-img-top p-2"
+                    />
+                  </div>
+                </Link>
+              </div>
+            </div>
           </div>
-        ))}
+        ))} */}
       </div>
     );
   }
