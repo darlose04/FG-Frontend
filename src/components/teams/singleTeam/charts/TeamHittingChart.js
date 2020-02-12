ef885cbbcfcb70d7a1a5caf642c51f5c0d8aacb2
Zach Smith <zsmith126028@gmail.com>
2020-02-12 11:20:24 -0600

    create initial chart component

---
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TeamHittingChart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This component will display stats in chart form</h1>
+      </div>
+    );
+  }
+}
