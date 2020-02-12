e5ee043a2c29c92f7728cabf47353d4c9ab46373
Zach Smith <zsmith126028@gmail.com>
2020-02-12 11:48:29 -0600

    install react-chartjs

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import Chart from "chart.js";
 
 export default class TeamHittingChart extends Component {
   render() {
     const { data } = this.props;
     console.log(data);
+
     return (
       <div>
         <h1>This component will display stats in chart form</h1>
+        <canvas id="myChart" width="400" height="400"></canvas>
       </div>
     );
   }
