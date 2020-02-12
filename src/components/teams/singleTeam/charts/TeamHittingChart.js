a432bebbf799322cfab1628a0c27883dd18ae28d
Zach Smith <zsmith126028@gmail.com>
2020-02-12 14:34:05 -0600

    add singles, doubles, and triples to chart with homers

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -5,6 +5,21 @@ export default class TeamHittingChart extends Component {
   render() {
     const { data } = this.props;
 
+    let singles = [];
+    data.map(year => {
+      return singles.unshift(year.singles);
+    });
+
+    let doubles = [];
+    data.map(year => {
+      return doubles.unshift(year.doubles);
+    });
+
+    let triples = [];
+    data.map(year => {
+      return triples.unshift(year.triples);
+    });
+
     let homers = [];
     data.map(year => {
       return homers.unshift(year.home_runs);
@@ -32,44 +47,84 @@ export default class TeamHittingChart extends Component {
         "2019"
       ],
       datasets: [
-        // {
-        //   label: "Hits",
-        //   fill: false,
-        //   lineTension: 0.1,
-        //   backgroundColor: "rgba(0,64,255,1)",
-        //   borderColor: "rgba(0,64,255,1)",
-        //   borderCapStyle: "butt",
-        //   borderDash: [],
-        //   borderDashOffset: 0.0,
-        //   borderJoinStyle: "miter",
-        //   pointBorderColor: "rgba(0,64,255,1)",
-        //   pointBackgroundColor: "#fff",
-        //   pointBorderWidth: 1,
-        //   pointHoverRadius: 5,
-        //   pointHoverBackgroundColor: "rgba(0,64,255,1)",
-        //   pointHoverBorderColor: "rgba(220,220,220,1)",
-        //   pointHoverBorderWidth: 2,
-        //   pointRadius: 1,
-        //   pointHitRadius: 10,
-        //   data: chartData.map(year => {
-        //     return year.hits;
-        //   })
-        // },
         {
-          label: "Home Runs",
+          label: "Singles",
           fill: false,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
+          backgroundColor: "#0040ff",
+          borderColor: "#0040ff",
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
+          pointBorderColor: "#0040ff",
           pointBackgroundColor: "#fff",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBackgroundColor: "#0040ff",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: singles
+        },
+        {
+          label: "Doubles",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#883341",
+          borderColor: "#883341",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "#883341",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "#883341",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: doubles
+        },
+        {
+          label: "Triples",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#28292e",
+          borderColor: "#28292e",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "#28292e",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "#28292e",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: triples
+        },
+        {
+          label: "Home Runs",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "#42aa75",
+          borderColor: "#42aa75",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "#42aa75",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "#42aa75",
           pointHoverBorderColor: "rgba(220,220,220,1)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
@@ -81,6 +136,7 @@ export default class TeamHittingChart extends Component {
 
     return (
       <div className="mb-3">
+        <h4 className="text-center">Hit Type</h4>
         <Line data={info} />
       </div>
     );
