53b9c3dcc83a4c50065e8d29f8baeb40d1fc8ae9
Zach Smith <zsmith126028@gmail.com>
2020-02-12 12:14:15 -0600

    add data for hits to chart. make new reversed copy of data array

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -5,14 +5,7 @@ export default class TeamHittingChart extends Component {
   render() {
     const { data } = this.props;
     console.log(data);
-
-    let homers = [];
-
-    data.map(year => {
-      homers.unshift(year.home_runs);
-    });
-
-    console.log(homers);
+    let chartData = data.reverse();
 
     const info = {
       labels: [
@@ -37,6 +30,29 @@ export default class TeamHittingChart extends Component {
       ],
       datasets: [
         {
+          label: "Hits",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(0,64,255,1)",
+          borderColor: "rgba(0,64,255,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(0,64,255,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(0,64,255,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: chartData.map(year => {
+            return year.hits;
+          })
+        },
+        {
           label: "Home Runs",
           fill: false,
           lineTension: 0.1,
@@ -55,14 +71,15 @@ export default class TeamHittingChart extends Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: homers
+          data: chartData.map(year => {
+            return year.home_runs;
+          })
         }
       ]
     };
 
     return (
       <div className="mb-3">
-        <h1>This component will display stats in chart form</h1>
         <Line data={info} />
       </div>
     );
