44054e543a7e46e530f836cc6dcd94c4a7191e91
Zach Smith <zsmith126028@gmail.com>
2020-02-12 11:28:38 -0600

    add chart button and props

--- src/components/teams/singleTeam/TeamPageBtns.js
+++ src/components/teams/singleTeam/TeamPageBtns.js
@@ -6,20 +6,34 @@ export default class TeamPageBtns extends Component {
       hittingBtnClass,
       pitchingBtnClass,
       showHitting,
-      showPitching
+      showPitching,
+      showChart
     } = this.props;
 
     return (
-      <div className="btn-group w-100 mb-3" role="group">
-        <button type="button" className={hittingBtnClass} onClick={showHitting}>
-          Hitting
-        </button>
+      <div className="mb-3">
+        <div className="btn-group w-100 mb-1" role="group">
+          <button
+            type="button"
+            className={hittingBtnClass}
+            onClick={showHitting}
+          >
+            Hitting
+          </button>
+          <button
+            type="button"
+            className={pitchingBtnClass}
+            onClick={showPitching}
+          >
+            Pitching
+          </button>
+        </div>
         <button
           type="button"
-          className={pitchingBtnClass}
-          onClick={showPitching}
+          className="w-100 btn btn-success"
+          onClick={showChart}
         >
-          Pitching
+          Display Charts
         </button>
       </div>
     );
