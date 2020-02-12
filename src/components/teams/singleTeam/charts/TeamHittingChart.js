a58c965251c2aea790ab701f4a7d3bbc92217af8
Zach Smith <zsmith126028@gmail.com>
2020-02-12 11:59:33 -0600

    add chart for displaying home runs

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -1,15 +1,69 @@
 import React, { Component } from "react";
-import Chart from "chart.js";
+import { Line } from "react-chartjs-2";
 
 export default class TeamHittingChart extends Component {
   render() {
     const { data } = this.props;
     console.log(data);
 
+    let homers = [];
+
+    data.map(year => {
+      homers.unshift(year.home_runs);
+    });
+
+    console.log(homers);
+
+    const info = {
+      labels: [
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017",
+        "2018",
+        "2019"
+      ],
+      datasets: [
+        {
+          label: "Home Runs",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: homers
+        }
+      ]
+    };
+
     return (
-      <div>
+      <div className="mb-3">
         <h1>This component will display stats in chart form</h1>
-        <canvas id="myChart" width="400" height="400"></canvas>
+        <Line data={info} />
       </div>
     );
   }
