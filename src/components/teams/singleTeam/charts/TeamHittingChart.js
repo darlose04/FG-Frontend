0d49a03576dade82f70ca5acb0476f5f9dbab156
Zach Smith <zsmith126028@gmail.com>
2020-02-14 09:24:59 -0600

    remove single from chart. change chart data to hits

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -25,7 +25,7 @@ export default class TeamHittingChart extends Component {
       return homers.unshift(year.home_runs);
     });
 
-    const info = {
+    const hits = {
       labels: [
         "2002",
         "2003",
@@ -47,27 +47,27 @@ export default class TeamHittingChart extends Component {
         "2019"
       ],
       datasets: [
-        {
-          label: "Singles",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "#0040ff",
-          borderColor: "#0040ff",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "#0040ff",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "#0040ff",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: singles
-        },
+        // {
+        //   label: "Singles",
+        //   fill: false,
+        //   lineTension: 0.1,
+        //   backgroundColor: "#0040ff",
+        //   borderColor: "#0040ff",
+        //   borderCapStyle: "butt",
+        //   borderDash: [],
+        //   borderDashOffset: 0.0,
+        //   borderJoinStyle: "miter",
+        //   pointBorderColor: "#0040ff",
+        //   pointBackgroundColor: "#fff",
+        //   pointBorderWidth: 1,
+        //   pointHoverRadius: 5,
+        //   pointHoverBackgroundColor: "#0040ff",
+        //   pointHoverBorderColor: "rgba(220,220,220,1)",
+        //   pointHoverBorderWidth: 2,
+        //   pointRadius: 1,
+        //   pointHitRadius: 10,
+        //   data: singles
+        // },
         {
           label: "Doubles",
           fill: false,
@@ -135,9 +135,9 @@ export default class TeamHittingChart extends Component {
     };
 
     return (
-      <div className="mb-3">
+      <div className="mb-3 w-50">
         <h4 className="text-center">Hit Type</h4>
-        <Line data={info} />
+        <Line data={hits} />
       </div>
     );
   }
