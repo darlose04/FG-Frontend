2c8657243f5555b6fc38c48f14437c51e274d762
Zach Smith <zsmith126028@gmail.com>
2020-02-17 15:34:09 -0600

    pass props into onClick and className

--- src/components/players/PlayerPageBtns.js
+++ src/components/players/PlayerPageBtns.js
@@ -2,15 +2,24 @@ import React, { Component } from "react";
 
 export default class PlayerPageBtns extends Component {
   render() {
+    const {
+      showHitters,
+      hitBtn,
+      showStarters,
+      startBtn,
+      showRelievers,
+      reliefBtn
+    } = this.props;
+
     return (
       <div className="btn-group w-100" role="group">
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className={hitBtn} onClick={showHitters}>
           Hitters
         </button>
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className={startBtn} onClick={showStarters}>
           Starters
         </button>
-        <button type="button" className="btn btn-outline-success">
+        <button type="button" className={reliefBtn} onClick={showRelievers}>
           Relievers
         </button>
       </div>
