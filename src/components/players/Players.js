a8cd7da5658fdf7116d4364f8037eed52cb28170
Zach Smith <zsmith126028@gmail.com>
2020-02-17 14:13:51 -0600

    add jumbotron to player page

--- src/components/players/Players.js
+++ src/components/players/Players.js
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 export default class Players extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the players page</h1>
+      <div className="mt-4">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4 text-success text-center">
+              Player Statistics
+            </h1>
+            <p className="lead text-center">
+              Display player statistics by season or search for a specific
+              player
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
