3576d1a398901ddff241c68025054e12d89c42b5
Zach Smith <zsmith126028@gmail.com>
2020-02-17 14:59:48 -0600

    create onClick from props

--- src/components/players/BtnChoice.js
+++ src/components/players/BtnChoice.js
@@ -4,10 +4,18 @@ export default class BtnChoice extends Component {
   render() {
     return (
       <div className="btn-group w-100" role="group">
-        <button type="button" className="btn btn-outline-success">
+        <button
+          type="button"
+          className="btn btn-outline-success"
+          onClick={this.props.showSearchForm}
+        >
           Search for a Specific Player
         </button>
-        <button type="button" className="btn btn-outline-success">
+        <button
+          type="button"
+          className="btn btn-outline-success"
+          onClick={this.props.showStatsBtns}
+        >
           Return Player Stats by Season
         </button>
       </div>
