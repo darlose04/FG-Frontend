252823c7dd1c28f8478dfb7a42b550835fbd2aa4
Zach Smith <zsmith126028@gmail.com>
2020-02-17 14:39:46 -0600

    add basic button group

---
+++ src/components/players/PlayerPageBtns.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class PlayerPageBtns extends Component {
+  render() {
+    return (
+      <div className="btn-group w-100" role="group">
+        <button type="button" className="btn btn-outline-success">
+          Hitting
+        </button>
+        <button type="button" className="btn btn-outline-success">
+          Starters
+        </button>
+        <button type="button" className="btn btn-outline-success">
+          Relievers
+        </button>
+      </div>
+    );
+  }
+}
