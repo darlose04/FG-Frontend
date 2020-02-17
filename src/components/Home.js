1985a9db0cdebde3ee94dd14bd5a5dc15c76976f
Zach Smith <zsmith126028@gmail.com>
2020-02-17 10:27:45 -0600

    remove jumbotron

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,8 @@
 import React, { Component } from "react";
+import "../styles/Home.css";
 
 export default class Home extends Component {
   render() {
-    return (
-      <div>
-        <div className="jumbotron jumbotron-fluid">
-          <div className="container">
-            <h1 className="display-4">
-              THIS WEBSITE IS STILL UNDER CONSTRUCTION
-            </h1>
-            <p className="lead">
-              Much of this site is still being developed and is incomplete.
-            </p>
-          </div>
-        </div>
-      </div>
-    );
+    return <div className="homePage"></div>;
   }
 }
