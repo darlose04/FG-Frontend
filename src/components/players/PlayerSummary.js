16e2c9aa8cc0b2c29f5a8858a0f10244fa7b6635
Zach Smith <zsmith126028@gmail.com>
2020-02-18 10:43:01 -0600

    create initial component for single player page

---
+++ src/components/players/PlayerSummary.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PlayerSummary extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This page will be for individual player stats</h2>
+      </div>
+    );
+  }
+}
