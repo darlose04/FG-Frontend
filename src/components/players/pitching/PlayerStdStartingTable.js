8ffa5ccb8520c43930e300144a65e2200e1951a2
Zach Smith <zsmith126028@gmail.com>
2020-02-18 08:56:30 -0600

    create table for starting pitching

---
+++ src/components/players/pitching/PlayerStdStartingTable.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+
+export default class PlayerStdStartingTable extends Component {
+  render() {
+    let {
+      stats,
+      showStd,
+      showAdv,
+      stdClass,
+      advClass,
+      clickStyle
+    } = this.props;
+
+    return (
+      <table
+        className="table table-sm table-striped table-bordered table-hover text-center standard"
+        style={{ fontSize: "0.8rem", padding: ".2rem", whiteSpace: "nowrap" }}
+      >
+        <thead>
+          <tr>
+            <th className={stdClass} onClick={showStd} style={clickStyle}>
+              Standard
+            </th>
+            <th className={advClass} onClick={showAdv} style={clickStyle}>
+              Advanced
+            </th>
+          </tr>
+        </thead>
+        <thead>
+          <tr className="bg-success text-white">
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">W</th>
+            <th scope="col">L</th>
+            <th scope="col">G</th>
+            <th scope="col">GS</th>
+            <th scope="col">IP</th>
+            <th scope="col">H</th>
+            <th scope="col">R</th>
+            <th scope="col">ER</th>
+            <th scope="col">HR</th>
+            <th scope="col">BB</th>
+            <th scope="col">SO</th>
+            <th scope="col">IBB</th>
+            <th scope="col">HBP</th>
+            <th scope="col">WP</th>
+            <th scope="col">GB</th>
+            <th scope="col">FB</th>
+            <th scope="col">LD</th>
+            <th scope="col">Balls</th>
+            <th scope="col">Strikes</th>
+            <th scope="col">Pitches</th>
+            <th scope="col">AVG</th>
+            <th scope="col">ERA</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats.map(info => (
+            <tr key={info.id}>
+              <td>{info.name}</td>
+              <td>{info.age}</td>
+              <td>{info.wins}</td>
+              <td>{info.losses}</td>
+              <td>{info.games}</td>
+              <td>{info.gs}</td>
+              <td>{info.ip}</td>
+              <td>{info.hits}</td>
+              <td>{info.runs}</td>
+              <td>{info.er}</td>
+              <td>{info.hr}</td>
+              <td>{info.bb}</td>
+              <td>{info.so}</td>
+              <td>{info.ibb}</td>
+              <td>{info.hbp}</td>
+              <td>{info.wp}</td>
+              <td>{info.gb}</td>
+              <td>{info.fb}</td>
+              <td>{info.ld}</td>
+              <td>{info.balls}</td>
+              <td>{info.strikes}</td>
+              <td>{info.pitches}</td>
+              <td>{info.avg}</td>
+              <td>{info.era}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
