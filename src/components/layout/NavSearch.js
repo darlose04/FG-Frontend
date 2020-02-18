4ac2472d79116a479e51646174be4f961d6a643f
Zach Smith <zsmith126028@gmail.com>
2020-02-18 12:36:40 -0600

    make search in navbar functional, same as player search component

--- src/components/layout/NavSearch.js
+++ src/components/layout/NavSearch.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 
 export default class NavSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchName: ""
+    };
+
+    this.searchValue = this.searchValue.bind(this);
+  }
+
+  searchValue(e) {
+    this.setState({
+      searchName: e.target.value
+    });
+  }
+
   render() {
     return (
-      <form className="form-inline ml-auto">
+      <form
+        action={`/players/${this.state.searchName}`}
+        className="form-inline ml-auto"
+      >
         <input
           type="text"
           className="form-control mr-2"
           placeholder="Search Players"
+          onChange={this.searchValue}
         />
         <button className="btn btn-outline-light">Search</button>
       </form>
