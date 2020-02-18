a11b1ef7dca8996f1fd592fbdbae5a6e3d1ef007
Zach Smith <zsmith126028@gmail.com>
2020-02-18 09:31:28 -0600

    create relief pitching component to show which relief table to display

---
+++ src/components/players/pitching/PlayerReliefComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export default class PlayerReliefComponent extends Component {
+  render() {
+    const {
+      std,
+      adv,
+      data,
+      showStd,
+      showAdv,
+      stdClass,
+      advClass,
+      clickStyle
+    } = this.props;
+
+    return (
+      <div>
+        {std ? (
+          <PlayerStdReliefTable
+            stats={data}
+            showStd={showStd}
+            showAdv={showAdv}
+            stdClass={stdClass}
+            advClass={advClass}
+            clickStyle={clickStyle}
+          />
+        ) : adv ? (
+          <PlayerAdvReliefTable
+            stats={data}
+            showStd={showStd}
+            showAdv={showAdv}
+            stdClass={stdClass}
+            advClass={advClass}
+            clickStyle={clickStyle}
+          />
+        ) : (
+          <div></div>
+        )}
+      </div>
+    );
+  }
+}
