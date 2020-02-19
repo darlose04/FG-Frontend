2e4961b9191692b68c1acebb464d3bd40710e751
Zach Smith <zsmith126028@gmail.com>
2020-02-19 12:17:53 -0600

    remove ternary and props

--- src/components/players/singlePlayer/pitching/ReliefComponent.js
+++ src/components/players/singlePlayer/pitching/ReliefComponent.js
@@ -4,40 +4,13 @@ import AdvReliefTable from "./AdvReliefTable";
 
 export default class ReliefComponent extends Component {
   render() {
-    const {
-      data,
-      std,
-      adv,
-      stdClass,
-      advClass,
-      showStd,
-      showAdv,
-      clickStyle
-    } = this.props;
+    const { data } = this.props;
 
     return (
       <div>
-        {std ? (
-          <StdReliefTable
-            stats={data}
-            showStd={showStd}
-            showAdv={showAdv}
-            stdClass={stdClass}
-            advClass={advClass}
-            clickStyle={clickStyle}
-          />
-        ) : adv ? (
-          <AdvReliefTable
-            stats={data}
-            showStd={showStd}
-            showAdv={showAdv}
-            stdClass={stdClass}
-            advClass={advClass}
-            clickStyle={clickStyle}
-          />
-        ) : (
-          <div></div>
-        )}
+        <StdReliefTable stats={data} />
+
+        <AdvReliefTable stats={data} />
       </div>
     );
   }
