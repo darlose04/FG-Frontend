d09ea815dca88dbc6fd665488a7a2bc04f52ae19
Zach Smith <zsmith126028@gmail.com>
2020-02-19 13:43:12 -0600

    remove clickStyle

--- src/components/players/singlePlayer/PlayerSummary.js
+++ src/components/players/singlePlayer/PlayerSummary.js
@@ -55,10 +55,6 @@ export default class PlayerSummary extends Component {
   render() {
     let playerName = this.props.match.params.player;
 
-    let clickStyle = {
-      cursor: "pointer"
-    };
-
     const { playerHitting, playerStarting, playerRelieving } = this.state;
 
     return (
