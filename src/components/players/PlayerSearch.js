fa109d71a7d0ca00d6a4694daa21ca130a80909f
Zach Smith <zsmith126028@gmail.com>
2020-02-20 10:12:05 -0600

    I have no idea how to make this work. I can't figure out how to make be affected by a regex of matching characters

--- src/components/players/PlayerSearch.js
+++ src/components/players/PlayerSearch.js
@@ -4,7 +4,8 @@ export default class PlayerSearch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchName: ""
+      searchName: "",
+      batters: this.props.batters
     };
 
     this.searchValue = this.searchValue.bind(this);
@@ -12,79 +13,68 @@ export default class PlayerSearch extends Component {
   }
 
   searchValue(e) {
+    const regex = new RegExp(`^${e.target.value}`, "gi");
+
     this.setState({
       searchName: e.target.value
     });
   }
 
   async searchPlayers(searchText) {
-    const resBatters = await fetch(
-      "https://www.fgbaseballapi.com/api/playerbatting/2019"
-    );
-    const resStarters = await fetch(
-      "https://www.fgbaseballapi.com/api/playerstarting/2019"
-    );
-    const resRelievers = await fetch(
-      "https://www.fgbaseballapi.com/api/playerrelieving/2019"
-    );
-
-    const batters = await resBatters.json();
-    const starters = await resStarters.json();
-    const relievers = await resRelievers.json();
-
-    // const players = [];
-
-    // batters.map(batter => {
-    //   return players.push(batter);
-    // });
-    // starters.map(starter => {
-    //   return players.push(starter);
-    // });
-    // relievers.map(reliever => {
-    //   return players.push(reliever);
-    // });
-
-    console.log(batters);
-
+    // const resBatters = await fetch(
+    //   "https://www.fgbaseballapi.com/api/playerbatting/2019"
+    // );
+    // const resStarters = await fetch(
+    //   "https://www.fgbaseballapi.com/api/playerstarting/2019"
+    // );
+    // const resRelievers = await fetch(
+    //   "https://www.fgbaseballapi.com/api/playerrelieving/2019"
+    // );
+    // const batters = await resBatters.json();
+    // const starters = await resStarters.json();
+    // const relievers = await resRelievers.json();
     // get matches to current text input
-    let matches = batters.filter(batter => {
-      const regex = new RegExp(`^${searchText}`, "gi");
-      return batter.name.match(regex);
-    });
-
-    console.log(matches);
+    // let matches = batters.filter(batter => {
+    //   const regex = new RegExp(`^${searchText}`, "gi");
+    //   return batter.name.match(regex);
+    // });
+    // console.log(matches);
   }
 
   render() {
-    // const search = document.getElementById("playerSearch");
-    // const matchList = document.getElementById("match-list");
-    // this.searchPlayers();
-    // search.addEventListener("input", () =>
-    //   this.searchPlayers(this.state.searchName)
-    // );
     return (
-      <form action={`/players/${this.state.searchName}`} className="my-3">
-        <div className="form-row justify-content-center align-items-center">
-          <div className="col-sm-4">
-            <label className="sr-only" htmlFor="playerSearch">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="playerSearch"
-              placeholder="Player Search"
-              onChange={this.searchValue}
-            />
+      <div>
+        <form action={`/players/${this.state.searchName}`} className="my-3">
+          <div className="form-row justify-content-center align-items-center">
+            <div className="col-sm-4">
+              <label className="sr-only" htmlFor="playerSearch">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="playerSearch"
+                placeholder="Player Search"
+                onChange={this.searchValue}
+              />
+            </div>
+
+            <div>
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </div>
           </div>
-          <div id="match-list"></div>
-          <div>
-            <button type="submit" className="btn btn-success">
-              Search
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
+        {this.state.batters.filter(batter => {
+          const regex = new RegExp(`^${this.state.searchName}`, "gi");
+          return (
+            <div key={batter.id}>
+              <h5>{batter.name.match(regex)}</h5>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
