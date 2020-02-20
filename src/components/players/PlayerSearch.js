79eb183c03fd277f20d023f99a018645028b39e8
Zach Smith <zsmith126028@gmail.com>
2020-02-20 09:12:51 -0600

    fetching all player data and using a filter to search it

--- src/components/players/PlayerSearch.js
+++ src/components/players/PlayerSearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
 
 export default class PlayerSearch extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class PlayerSearch extends Component {
     };
 
     this.searchValue = this.searchValue.bind(this);
+    this.searchPlayers = this.searchPlayers.bind(this);
   }
 
   searchValue(e) {
@@ -16,7 +18,40 @@ export default class PlayerSearch extends Component {
     });
   }
 
+  async searchPlayers(searchText) {
+    const resBatters = await fetch(
+      "https://www.fgbaseballapi.com/api/playerbatting"
+    );
+    const resStarters = await fetch(
+      "https://www.fgbaseballapi.com/api/playerstarting"
+    );
+    const resRelievers = await fetch(
+      "https://www.fgbaseballapi.com/api/playerrelieving"
+    );
+    const batters = await resBatters.json();
+    const starters = await resStarters.json();
+    const relievers = await resRelievers.json();
+
+    const players = [];
+
+    batters.map(batter => {
+      return players.push(batter);
+    });
+    starters.map(starter => {
+      return players.push(starter);
+    });
+    relievers.map(reliever => {
+      return players.push(reliever);
+    });
+
+    // get matches to current text input
+    let matches = players.filter(player => {
+      const regex = new RegExp(`^${searchText}`, "gi");
+    });
+  }
+
   render() {
+    this.searchPlayers();
     return (
       <form action={`/players/${this.state.searchName}`} className="my-3">
         <div className="form-row justify-content-center align-items-center">
@@ -32,6 +67,7 @@ export default class PlayerSearch extends Component {
               onChange={this.searchValue}
             />
           </div>
+          <div id="match-list"></div>
           <div>
             <button type="submit" className="btn btn-success">
               Search
