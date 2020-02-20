2b1f94407d5ee92a628917c2ac63eea735a960d4
Zach Smith <zsmith126028@gmail.com>
2020-02-20 10:32:40 -0600

    may have made a positive step forward. put searchName into searchPlayers function as a paramter

--- src/components/players/PlayerSearch.js
+++ src/components/players/PlayerSearch.js
@@ -1,47 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PlayerSearch extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchName: "",
-      batters: this.props.batters
+      starters: []
     };
 
     this.searchValue = this.searchValue.bind(this);
     this.searchPlayers = this.searchPlayers.bind(this);
   }
 
-  searchValue(e) {
-    const regex = new RegExp(`^${e.target.value}`, "gi");
+  async componentDidMount() {
+    const res = await fetch(
+      "https://www.fgbaseballapi.com/api/playerstarting/2019"
+    );
 
+    const starters = await res.json();
+    console.log(starters);
+
+    this.setState({
+      starters: starters
+    });
+  }
+
+  searchValue(e) {
     this.setState({
       searchName: e.target.value
     });
   }
 
   async searchPlayers(searchText) {
-    // const resBatters = await fetch(
-    //   "https://www.fgbaseballapi.com/api/playerbatting/2019"
-    // );
-    // const resStarters = await fetch(
-    //   "https://www.fgbaseballapi.com/api/playerstarting/2019"
-    // );
-    // const resRelievers = await fetch(
-    //   "https://www.fgbaseballapi.com/api/playerrelieving/2019"
-    // );
-    // const batters = await resBatters.json();
-    // const starters = await resStarters.json();
-    // const relievers = await resRelievers.json();
     // get matches to current text input
-    // let matches = batters.filter(batter => {
-    //   const regex = new RegExp(`^${searchText}`, "gi");
-    //   return batter.name.match(regex);
-    // });
-    // console.log(matches);
+    let matches = this.state.starters.filter(starter => {
+      const regex = new RegExp(`^${searchText}`, "gi");
+      return starter.name.match(regex);
+    });
+    console.log(matches);
   }
 
   render() {
+    this.searchPlayers(this.state.searchName);
     return (
       <div>
         <form action={`/players/${this.state.searchName}`} className="my-3">
@@ -58,7 +59,6 @@ export default class PlayerSearch extends Component {
                 onChange={this.searchValue}
               />
             </div>
-
             <div>
               <button type="submit" className="btn btn-success">
                 Search
@@ -66,14 +66,6 @@ export default class PlayerSearch extends Component {
             </div>
           </div>
         </form>
-        {this.state.batters.filter(batter => {
-          const regex = new RegExp(`^${this.state.searchName}`, "gi");
-          return (
-            <div key={batter.id}>
-              <h5>{batter.name.match(regex)}</h5>
-            </div>
-          );
-        })}
       </div>
     );
   }
