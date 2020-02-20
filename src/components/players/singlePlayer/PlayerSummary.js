8d40fc9f230961797ce301dee5a263acc8e2dc95
Zach Smith <zsmith126028@gmail.com>
2020-02-20 13:46:59 -0600

    add loading to state and import Spinner component

--- src/components/players/singlePlayer/PlayerSummary.js
+++ src/components/players/singlePlayer/PlayerSummary.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import BattingComponent from "./batting/BattingComponent";
 import StartingComponent from "./pitching/StartingComponent";
 import ReliefComponent from "./pitching/ReliefComponent";
+import Spinner from "../../layout/Spinner";
 import logos from "../../../logos";
 
 import axios from "axios";
@@ -14,7 +15,8 @@ export default class PlayerSummary extends Component {
     this.state = {
       playerHitting: [],
       playerStarting: [],
-      playerRelieving: []
+      playerRelieving: [],
+      loading: true
     };
   }
 
@@ -48,49 +50,61 @@ export default class PlayerSummary extends Component {
     this.setState({
       playerHitting: sortedHitting,
       playerStarting: sortedStarting,
-      playerRelieving: sortedRelieving
+      playerRelieving: sortedRelieving,
+      loading: false
     });
   }
 
   render() {
     let playerName = this.props.match.params.player;
 
-    const { playerHitting, playerStarting, playerRelieving } = this.state;
+    const {
+      playerHitting,
+      playerStarting,
+      playerRelieving,
+      loading
+    } = this.state;
 
     return (
       <div>
-        {playerHitting.length === 0 &&
-        playerStarting.length === 0 &&
-        playerRelieving.length === 0 ? (
-          <div>
-            <h1 className="mt-3 mb-2 text-center">No Player Found</h1>
-            <h4 className="text-center">
-              <a className="text-success" href="/players">
-                Return to Search
-              </a>
-            </h4>
-          </div>
+        {loading ? (
+          <Spinner />
         ) : (
           <div>
-            <h1 className="my-3">{playerName}</h1>
-            {playerStarting.length > 0 ? (
+            {playerHitting.length === 0 &&
+            playerStarting.length === 0 &&
+            playerRelieving.length === 0 ? (
               <div>
-                <h3>Starting Pitching</h3>
-                <StartingComponent data={playerStarting} />
+                <h1 className="mt-3 mb-2 text-center">No Player Found</h1>
+                <h4 className="text-center">
+                  <a className="text-success" href="/players">
+                    Return to Search
+                  </a>
+                </h4>
               </div>
             ) : (
-              <div></div>
-            )}
-            {playerRelieving.length > 0 ? (
               <div>
-                <h3>Relief Pitching</h3>
-                <ReliefComponent data={playerRelieving} />
+                <h1 className="my-3">{playerName}</h1>
+                {playerStarting.length > 0 ? (
+                  <div>
+                    <h3>Starting Pitching</h3>
+                    <StartingComponent data={playerStarting} />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+                {playerRelieving.length > 0 ? (
+                  <div>
+                    <h3>Relief Pitching</h3>
+                    <ReliefComponent data={playerRelieving} />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+                <h3>Hitting</h3>
+                <BattingComponent data={playerHitting} />
               </div>
-            ) : (
-              <div></div>
             )}
-            <h3>Hitting</h3>
-            <BattingComponent data={playerHitting} />
           </div>
         )}
       </div>
