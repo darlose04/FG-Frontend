bb2277de88b7d011fcf3141a8693a15d624cb4fc
Zach Smith <zsmith126028@gmail.com>
2020-02-21 09:34:39 -0600

    create array for seasons to be labels instead of typing out labels

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -5,6 +5,11 @@ export default class TeamHittingChart extends Component {
   render() {
     const { data } = this.props;
 
+    let season = [];
+    data.map(year => {
+      return season.unshift(year.season);
+    });
+
     let singles = [];
     data.map(year => {
       return singles.unshift(year.singles);
@@ -26,26 +31,7 @@ export default class TeamHittingChart extends Component {
     });
 
     const hits = {
-      labels: [
-        "2002",
-        "2003",
-        "2004",
-        "2005",
-        "2006",
-        "2007",
-        "2008",
-        "2009",
-        "2010",
-        "2011",
-        "2012",
-        "2013",
-        "2014",
-        "2015",
-        "2016",
-        "2017",
-        "2018",
-        "2019"
-      ],
+      labels: season,
       datasets: [
         // {
         //   label: "Singles",
