bfebade32e35993f3c9b8c8a746e3363b6064380
Zach Smith <zsmith126028@gmail.com>
2020-02-24 08:48:27 -0600

    remove singles commented code

--- src/components/teams/singleTeam/charts/TeamHittingChart.js
+++ src/components/teams/singleTeam/charts/TeamHittingChart.js
@@ -33,27 +33,6 @@ export default class TeamHittingChart extends Component {
     const hits = {
       labels: season,
       datasets: [
-        // {
-        //   label: "Singles",
-        //   fill: false,
-        //   lineTension: 0.1,
-        //   backgroundColor: "#0040ff",
-        //   borderColor: "#0040ff",
-        //   borderCapStyle: "butt",
-        //   borderDash: [],
-        //   borderDashOffset: 0.0,
-        //   borderJoinStyle: "miter",
-        //   pointBorderColor: "#0040ff",
-        //   pointBackgroundColor: "#fff",
-        //   pointBorderWidth: 1,
-        //   pointHoverRadius: 5,
-        //   pointHoverBackgroundColor: "#0040ff",
-        //   pointHoverBorderColor: "rgba(220,220,220,1)",
-        //   pointHoverBorderWidth: 2,
-        //   pointRadius: 1,
-        //   pointHitRadius: 10,
-        //   data: singles
-        // },
         {
           label: "Doubles",
           fill: false,
@@ -184,10 +163,10 @@ export default class TeamHittingChart extends Component {
           <h4 className="text-center">Hit Type</h4>
           <Line data={hits} />
         </div>
-        <div className="mb-3 col">
+        {/* <div className="mb-3 col">
           <h4 className="text-center">Scoring</h4>
           <Line data={scoring} />
-        </div>
+        </div> */}
       </div>
     );
   }
