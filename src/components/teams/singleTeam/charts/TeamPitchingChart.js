3cde2fb23ccc67c4a79eade33b1e051f16a0d509
Zach Smith <zsmith126028@gmail.com>
2020-02-24 11:36:02 -0600

    create initial pitching chart component

---
+++ src/components/teams/singleTeam/charts/TeamPitchingChart.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TeamPitchingChart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This will be the team pitching chart</h1>
+      </div>
+    );
+  }
+}
